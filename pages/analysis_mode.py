1be441e36c5ca74aebe9b0c5d7400f385d12c127
DataCop07 <mohitahire2005@gmail.com>
2026-03-02 18:31:22 +0530

    Update analysis_mode.py

--- pages/analysis_mode.py
+++ pages/analysis_mode.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="UPI Transactions Analysis Mode", layout="wide")
 # Load CSV
 # ===========================
 try:
-    df = pd.read_csv(r"C:\Users\HP\OneDrive\Desktop\project\upi_transactions_2024.csv")
+    df = pd.read_csv(r"upi_transactions_2024.csv")
 except FileNotFoundError:
     st.error("File upi_transactions_2024.csv not found! Please check the path.")
     st.stop()
